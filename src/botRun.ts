1033d7e79eee968ac058d620c36f12f46ca2fb41
jiahui <clash9529@gmail.com>
2023-03-28 18:55:16 +0800

    pushh

--- src/botRun.ts
+++ src/botRun.ts
@@ -1,6 +1,6 @@
 const PythonShell = require('python-shell').PythonShell;
 
-PythonShell.run('bot.py', null, function (err) {
+PythonShell.run('bot.py', null, function (err:any) {
   if (err) throw err;
   console.log('finished');
 });
