d4d6c36c9cfa9b4a4d1fd431a0ae6924ab04c71e
clash9529 <clash9529@gmail.com>
2023-05-03 01:55:56 +0800

    add

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ from telebot import formatting
 from flask import Flask, request
 
 
+API_KEY=os.environ.get('API_KEY')
 
-
-bot = telebot.TeleBot(os.environ.get('API_KEY'))
+bot = telebot.TeleBot(API_KEY)
 bot.delete_webhook()
 
 server = Flask(__name__)
@@ -298,25 +298,25 @@ def riddle(message):
         parse_mode='HTML'
     )
 
-bot.polling()
+# bot.polling()
 
 ############################## SERVER ###############################
 
 
-# @server.route('/' + API_KEY, methods=['POST'])
-# def getMessage():
-#     json_string = request.get_data().decode('utf-8')
-#     update = telebot.types.Update.de_json(json_string)
-#     bot.process_new_updates([update])
-#     return "!", 200
+@server.route('/' + API_KEY, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
 
 
-# @server.route("/")
-# def webhook():
-#     bot.remove_webhook()
-#     bot.set_webhook(url='https://musical-octo-eureka.herokuapp.com/' + API_KEY)
-#     return "!", 200
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://musical-octo-eureka.herokuapp.com/' + API_KEY)
+    return "!", 200
 
 
-# if __name__ == "__main__":
-#     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
